33e13d9976b3063737b0ef4ecff3a4a8bc7e92fe
pascual-francisco <69876603+pascual-francisco@users.noreply.github.com>
2023-04-15 00:40:59 -0400

    Version 6.0.0

--- TextureManager.h
+++ TextureManager.h
@@ -10,7 +10,6 @@ class TextureManager
 	__declspec(dllexport)		TextureManager();
 	__declspec(dllexport)		~TextureManager();
 	__declspec(dllexport) void	initTexture(GLuint name, string path, GLuint quantity, GLuint target);
-	__declspec(dllexport) void	initTileSet(GLuint name, string path, GLuint tileWidth, GLuint tileHeight, GLuint columns, GLuint rows, GLuint columnOffset, GLuint rowOffset, GLuint target);
 	__declspec(dllexport) void print() const;
 };
 #endif // !TEXTUREMANAGER_H
